8ca60286781122b7e612a9ccfc6b3bb56c19940a
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-12-11 00:27:03 +0530

    checking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,9 +23,9 @@ function App() {
   return (
     <>
       <div
-        className="min-h-screen min-w-screen border flex justify-center items-center border-black relative dark:bg-black text-red-500"
+        className="min-h-screen transition-all duration-200 ease-linear min-w-screen border flex justify-center items-center border-black relative dark:bg-black dark:text-white"
       >
-        <h1 className="text-5xl font-bold">Campus Connect</h1>
+        <h1 className="text-5xl font-bold bg-red-500 dark:bg-red-800  px-6 py-6 rounded-md">Campus Connect</h1>
         <button
           className="absolute top-5 right-5 p-2 bg-gray-200 rounded-md shadow-md"
           onClick={handleMode}
