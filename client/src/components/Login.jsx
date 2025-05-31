3d3fcb5595990a5eedf00002e8dd3fa9c94f1a9b
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2025-05-31 12:50:46 +0530

    updated

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -31,7 +31,7 @@ function Login() {
       const res = await axios.post(`${url}/login`, loginData, {
         withCredentials: true
       })
-      console.log(user)
+      const user = res.data.user
       localStorage.setItem("isLoggedin", true);
       localStorage.setItem("loggedInUser", JSON.stringify(user));
       localStorage.setItem("token", res.data.token);
